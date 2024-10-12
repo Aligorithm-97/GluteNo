dafb780566f4159e54b4385b9ef7cd282644a171
Aligorithm-97 <atemurtas97@gmail.com>
2024-10-12 20:13:06 +0300

    aligorithm-projectStructure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,15 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>GluteNot</div>
-    </>
+    <div style={{ color: "white" }}>
+      <div>Seach bar</div>
+      <div>Foods in cards</div>
+      <div>calori - glutenFree or not</div>
+      <div>infinite scroll</div>
+      <div>create a free food api and deploy</div>
+      <div>first version does not include login or any other service</div>
+      <div>main purpose is api</div>
+    </div>
   );
 }
 
