416cac9e39b24cbe2bff5c366957216517c3226a
Aligorithm-97 <atemurtas97@gmail.com>
2024-11-27 15:01:38 +0300

    aligorithm-todo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,12 @@ function App() {
     <div style={{ color: "white" }}>
       <input type="text" />
       <div>Seach bar</div>
-      <div>Foods in cards</div>
       <div>calori - glutenFree or not</div>
       <div>infinite scroll</div>
       <div>create a free food api and deploy</div>
       <div>first version does not include login or any other service</div>
-      <div>main purpose is api</div>
+      <div>mongo</div>
       <div>elastic search</div>
-      <div></div>
     </div>
   );
 }
