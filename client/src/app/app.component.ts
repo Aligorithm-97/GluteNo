596a0c242aa33b71789d546733451bdc71812277
Aligorithm-97 <atemurtas97@gmail.com>
2024-11-30 15:31:51 +0300

    aligorithm-css

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common'; 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatCardModule, MatButtonModule],
+  imports: [RouterOutlet,MatCardModule, MatButtonModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  items = Array.from({ length: 20 }, (_, index) => ({
+    title: `White Bread ${index + 1}`,
+    subtitle: 'Bread',
+    image: 'assets/white.png',
+    altText: 'Photo of White Bread',
+    description: 'X Contains gluten X'
+  }));
   title = 'cli';
 }
